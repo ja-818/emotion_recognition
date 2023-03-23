2dc0298781a6e8df268299da16c4007be8118275
ja818 <julianarango1818@gmail.com>
2023-03-23 01:14:22 -0500

    Integrated html

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from ml import sentiment_analysis
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def root():
-    return "Holo bolo"
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("index.html", context)
+
 
 @app.get("/sentiment")
 def return_sentiment_analysis(prompt: str):
