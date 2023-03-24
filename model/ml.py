e471060bbc7a9c86539a4f5bdf5f2e62ae12fc63
ja818 <julianarango1818@gmail.com>
2023-03-24 06:08:23 -0500

    Cleaned ml code

--- model/ml.py
+++ model/ml.py
@@ -1,8 +1,8 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-def sentiment_analysis(sentence):
+def predict(review):
     sid_obj = SentimentIntensityAnalyzer()
-    sentiment_dict = sid_obj.polarity_scores(sentence)
+    sentiment_dict = sid_obj.polarity_scores(review)
     sentiment = ""
     
     if sentiment_dict['compound'] >= 0.05 :
